b284517fccd6f832508668df1b9a2a6f3c0ba363
janbe30 <janbe30@gmail.com>
2021-06-10 22:35:07 -0500

    Initial file structure and UI

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// Set up gameboard : Gameboard object using Revealing Module Pattern
+const Gameboard = (() => { 
+    const gameboard = [];
+    // _privateMethods
+
+    // public methods 
+
+    return {};
+})(); //IIFE 
+
+// Set up players : Players objects using factory fns
+const Player = (name, score) => {
+    // methods
+
+    return {};
+}
